a7eba10ae9465bd9e5079ad90850c8aaea00b4fa
bBibyk <125393249+bBibyk@users.noreply.github.com>
2023-03-28 16:56:58 +0200

    Création classe Humain, et première histoire.

---
+++ src/personnages/Humain.java
@@ -0,0 +1,52 @@
+package personnages;
+
+public class Humain {
+	private String nom;
+	private String boisson;
+	private int argent;
+
+	public Humain(String nom, String boisson, int argent) {
+		super();
+		this.nom = nom;
+		this.boisson = boisson;
+		this.argent = argent;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public int getArgent() {
+		return argent;
+	}
+
+	private void parler(String texte) {
+		System.out.println("(" + nom + ")" + " - " + texte);
+	}
+
+	public void direBonjour() {
+		parler("Bonjour ! Je m’appelle " + nom + " et j’aime boire du " + boisson);
+	}
+
+	public void boire() {
+		parler("Mmmm, un bon verre de " + boisson + " ! GLOUPS !");
+	}
+
+	public void gagnerArgent(int gain) {
+		argent = argent + gain;
+	}
+
+	public void perdreArgent(int perte) {
+		assert(perte<=argent);
+		argent = argent - perte;
+	}
+
+	public void acheter(String bien, int prix) {
+		if(prix<=argent) {
+			perdreArgent(prix);
+			parler("J'ai " + argent + " sous en poche. Je vais pouvoir m'offrir " + bien + " à " + prix + " sous.");
+		}else {
+			parler("Je n'ai plus que " + argent + "sous en poche. Je ne peux pas m'offrir " + bien + " à " + prix + " sous.");
+		}
+	}
+}
